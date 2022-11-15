67fd66f44527c2615203dd7d3124e9b7f93e6291
Peter van Heusden <pvh@sanbi.ac.za>
2022-11-15 22:10:36 +0200

    Add organism column

--- streamlit-primers/primer_schemes.py
+++ streamlit-primers/primer_schemes.py
@@ -10,6 +10,8 @@ import yaml
 
 from manifest import PrimerSchemeManifest
 
+st.set_page_config(layout="wide")
+
 @st.cache
 def fetch_primer_list(schemelist_url: str = 'https://raw.githubusercontent.com/pha4ge/primer-schemes/main/index.yml') -> PrimerSchemeManifest:
     response = requests.get(schemelist_url)
@@ -32,11 +34,11 @@ tab1.subheader('Primer Schemes')
 if len(organisms) > 1:
     tab1.multiselect('Organisms', organisms, key='selected_organisms')
     tab1.write(st.session_state.selected_organisms)
-table_md = ['|Name|Latest version|Available versions', '|--|--|--|']
+table_md = ['|Name|Organism|Latest version|Available versions', '|--|--|--|--|']
 for scheme in primer_list.schemes:
     if scheme.organism in st.session_state.selected_organisms:
         bold_if = lambda v: '**' if v.version == scheme.latest_version else ''
         scheme_versions_md = ', '.join([ f'{bold_if(v)}[{v.version}]({v.repository}){bold_if(v)}' for v in scheme.versions ])
-        table_md.append(f'|{scheme.name}|{scheme.latest_version}|{scheme_versions_md}|')
+        table_md.append(f'|{scheme.name}|{scheme.organism}|{scheme.latest_version}|{scheme_versions_md}|')
 tab1.markdown('\n'.join(table_md))
 
